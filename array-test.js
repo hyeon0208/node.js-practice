620f8d749e033ad4498b817d8075b199ddfe6432
SuhHyeonjun <dmsgktn0208@gmail.com>
2022-07-18 18:36:38 +0900

    move to js repo

--- array-test.js
+++
@@ -1,9 +0,0 @@
-const coffee = [];
-
-coffee.push({name: "americano" });
-coffee.push({name: "latte" });
-
-
-console.log(coffee); //  {name: "americano" }, {name: "latte" } 
-console.log(coffee[0]); // {name: "americano}
-console.log(coffee.length;) // 2
