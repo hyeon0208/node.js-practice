82f0f5f12ec1120c6f46baf16682cbf2afc32a24
SuhHyeonjun <dmsgktn0208@gmail.com>
2022-07-18 18:30:43 +0900

    array test code

---
+++ array-test.js
@@ -0,0 +1,9 @@
+const coffee = [];
+
+coffee.push({name: "americano" });
+coffee.push({name: "latte" });
+
+
+console.log(coffee); //  {name: "americano" }, {name: "latte" } 
+console.log(coffee[0]); // {name: "americano}
+console.log(coffee.length;) // 2
