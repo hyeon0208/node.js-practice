9c4daeac4e224cce33a7ae87df637b62769ebaac
SuhHyeonjun <dmsgktn0208@gmail.com>
2022-07-20 15:45:43 +0900

    동기 처리 시간 결과

---
+++ javascripts/asynchronous/synch-test3.js
@@ -0,0 +1,25 @@
+function work(sec, callback) {
+    setTimeout(() => {
+   callback(new Date().toISOString())
+}, sec * 1000)
+}
+
+work(1, (result) => {
+   console.log("첫 번째 작업", result);
+
+        work(1, (result) => {
+
+            work(1, (result) => {
+            console.log("세 번째 작업", result);
+            })
+
+            console.log("두 번째 작업", result)
+        })
+});
+
+/* 비동기 처리시
+첫 번째 작업 2022-07-20T06:32:57.164Z
+두 번째 작업 2022-07-20T06:32:57.167Z
+세 번째 작업 2022-07-20T06:32:57.167Z
+로 모든 같은 시간에 끝난다. 
+*/
