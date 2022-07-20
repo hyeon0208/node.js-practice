efeceedee396ed06c192229a57b6fe43a52cea25
SuhHyeonjun <dmsgktn0208@gmail.com>
2022-07-20 13:53:21 +0900

    callback 비동기

---
+++ javascripts/asynchronous/callback-test.js
@@ -0,0 +1,7 @@
+setTimeout(() => { setTimeout(callback, delayTime)
+    console.log('Todo: First work');
+    }, 3000);
+
+    setTimeout(() => {
+            console.log('Todo: Second work!');
+    }, 2000);
