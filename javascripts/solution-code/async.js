edaa9429d00daa45c2a8f82ffed202de1bfca013
SuhHyeonjun <dmsgktn0208@gmail.com>
2022-07-21 18:22:34 +0900

    setTimeout 내장함수. 비동기

---
+++ javascripts/solution-code/async.js
@@ -0,0 +1,11 @@
+console.log('First Console');
+
+setTimeout(function () {
+    console.log('Second Console');
+}, 0);
+
+console.log('Third Console');
+
+// First Console
+// Third Console
+// Second Console
