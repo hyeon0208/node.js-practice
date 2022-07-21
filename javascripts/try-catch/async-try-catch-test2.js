9e21b3d1e50840298f3b98a062310c9c55fb4c80
SuhHyeonjun <dmsgktn0208@gmail.com>
2022-07-21 17:51:20 +0900

    async 예외 처리2

---
+++ javascripts/try-catch/async-try-catch-test2.js
@@ -0,0 +1,12 @@
+async function AsyncFunc() {
+    throw "my func Error!";
+}
+
+function promiseFunc() {
+    return new Promise((resolve, reject) => {
+        reject("promise Fuc Error!");
+    });
+}
+
+const result = AsyncFunc().catch(e => {console.log(e)});
+const result2 = promiseFunc().catch(e => {console.log(e)});
