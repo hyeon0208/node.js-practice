ed7b889d0c80948d81bcb959467cf5f5064acb24
SuhHyeonjun <dmsgktn0208@gmail.com>
2022-07-21 17:31:35 +0900

    예외 처리 try/catch 사용

---
+++ javascripts/try-catch/try-catch-test2.js
@@ -0,0 +1,18 @@
+function f2() {
+    console.log("f2 함수 시작");
+    throw new Error("오류 발생");
+    console.log("f2 함수 끝");
+}
+
+function f1() {
+    console.log("f1 함수 시작");
+    try {
+        f2();
+    } catch (e) {
+        console.log(e);
+    }
+    console.log("f1 함수 끝")
+}
+
+f1();
+
