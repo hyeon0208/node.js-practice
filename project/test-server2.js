90897833036e0a7256b517edae66f4746332460c
SuhHyeonjun <dmsgktn0208@gmail.com>
2022-07-26 21:49:29 +0900

    htpp 모듈 테스트 서버

---
+++ project/test-server2.js
@@ -0,0 +1,11 @@
+const http = require('http');
+
+http.createrServer((req, res) => {
+    res.writeHead(200, { "content-Type: test/html; charset=utf-8" });
+    res.write( "<h1>Node.js로 서버 만들기</h1>");
+    res.end("<p>http모듈 공부 중...</p>")
+})
+
+    .listen(8080, () => {
+        console.log("8080포트에서 서버 연결 중...")
+    });
