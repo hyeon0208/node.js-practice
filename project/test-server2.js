599ae010dd7cf134fd0517369b84f1bf699d37fa
SuhHyeonjun <dmsgktn0208@gmail.com>
2022-07-27 16:38:23 +0900

    FIX:createServer

--- project/test-server2.js
+++ project/test-server2.js
@@ -1,6 +1,6 @@
 const http = require('http');
 
-http.createrServer((req, res) => {
+http.createServer((req, res) => {
     res.writeHead(200, { "content-Type": "test/html; charset=utf-8" });
     res.write( "<h1>Node.js로 서버 만들기</h1>");
     res.end("<p>http모듈 공부 중...</p>")
