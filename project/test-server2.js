25e0b1b0c2912411ef35f9402d95012204e19fbc
SuhHyeonjun <dmsgktn0208@gmail.com>
2022-07-27 16:46:49 +0900

    FIX:test/html => text/html

--- project/test-server2.js
+++ project/test-server2.js
@@ -1,14 +1,15 @@
 const http = require('http');
 
 http.createServer((req, res) => {
-    res.writeHead(200, { "content-Type": "test/html; charset=utf-8" });
-    res.write( "<h1>Node.js로 서버 만들기</h1>");
+    res.writeHead(200, { "Content-Type": "text/html; charset=utf-8" });
+    res.write("<h1>Node.js로 서버 만들기</h1>");
     res.end("<p>http모듈 공부 중...</p>")
 })
 
     .listen(8080, () => {
         console.log("8080포트에서 서버 연결 중...")
     });
+
 
 
 
