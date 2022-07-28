cb7f9f8ac56cbaca07959c5bd448aef92f895aa0
SuhHyeonjun <dmsgktn0208@gmail.com>
2022-07-28 13:44:13 +0900

    use static middleware

--- express/express-test6.js
+++ express/express-test6.js
@@ -6,7 +6,7 @@ app.set('port', process.env.PORT || 8080);
 app.use(express.static(__dirname + '/static'));
 
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/index.html');
+    res.sendFile(__dirname + '/index3.html');
 });
 
 app.listen(app.get('port'), () => {
