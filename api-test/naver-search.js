ab82d9d22433a77f3ef0da5a77d750d7ec2ac4c7
SuhHyeonjun <dmsgktn0208@gmail.com>
2022-07-29 17:44:52 +0900

    naver 검색 오픈소스 활용

---
+++ api-test/naver-search.js
@@ -0,0 +1,65 @@
+const morgan = require('morgan');
+const request = require('request');
+const express = require('express');
+const app = express();
+
+/* 포트 설정 */
+app.set('port', process.env.PORT || 8080);
+
+/* 공통 미들웨어 */
+app.use(morgan('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// 라우팅 설정
+app.get('/naver/news', (req, res) => {
+    const client_id = '5mAMc3kiYw5F31M6iqUV';
+    const client_secret = 'iHuHSuWv3g';
+    const api_url = 'https://openapi.naver.com/v1/search/news?query=' + encodeURI('코스피'); //encodeURI(req.query.query);'
+    const option = {
+    };
+    const options = {
+        url: api_url,
+        qs: option,
+        Headers: { 'X-Naver-Client-Id': client_id, 'X-Naver-Client-Secret': client_secret }
+    }
+
+
+request.get(option, (error, response, body) => {
+    if (!error && response.statusCode == 200) {
+        let newsItem = JSON.parse(body).items;
+    
+
+    const newsjson = {
+        title: [],
+        link: [],
+        descrption: [],
+        pubData: []
+    }
+
+
+    for (leti  = 0; i < newsItem.length; i++) {
+        newsJson.title.push(newsItem[i].title.replace(/(<([^>]+)>)|&quot;/ig, ""));
+        newsJson.link.push(newsItem[i].link);
+        newsJson.description.push(newsItem[i].description.replace(/(<([^>]+)>)|&quot;/ig, ""));
+        /*
+        replace 함수는 문자열을 치환해주는 역할 (치환할 문자열, 치환 후 문자열)
+         => 여기선 html 문자열을 지워주기 위해 사용 
+        */
+        newsJson.pubDate.push(newsItem[i].pubDate);
+    }
+    res.json(newsJson);
+
+    } else {
+    res.status(response.statusCode).end();
+    console.log('error = ' + response.statusCode);
+    }       
+
+    });
+
+});
+
+/* 서버와 포트 연결.. */
+app.listen(app.get('port'), () => {
+    console.log(app.get('port'), '번 포트에서 서버 실행 중 ..')
+});
