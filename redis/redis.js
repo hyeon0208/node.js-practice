0e28b4078b2a8be2dbcf77ef46e61e269d593e26
SuhHyeonjun <dmsgktn0208@gmail.com>
2022-08-04 17:21:23 +0900

    4이상버전 코드 추가

--- redis/redis.js
+++ redis/redis.js
@@ -7,3 +7,17 @@ const client = redis.createClient(6379, '127.0.0.1');
 client.get('myKey', (err, value) => {
     console.log(value);
 });
+
+// 4 이상 버전.
+//async function run(){
+//     await client.connect()
+// }
+
+// run();
+
+// async function getVal(key){
+//     const item = await client.lRange(key, 0, -1);
+//     console.log(item);
+// }
+
+// getVal("myKey");
