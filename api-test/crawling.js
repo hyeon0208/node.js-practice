ee23253129ff4427344a12c7a7758b0fddee03a1
SuhHyeonjun <dmsgktn0208@gmail.com>
2022-08-08 17:00:14 +0900

    ADD: 주석 설명

--- api-test/crawling.js
+++ api-test/crawling.js
@@ -15,10 +15,10 @@ getHtml()
         const $ = cheerio.load(html.data); // 받은 html 데이터를 cheerio 객체로 변환.
         const $bodyList = $("div#searchList ol").children("li"); //갖고자하는 부분을 변수에 할당.
 
-        $bodyList.each(function (i, elem) {
+        $bodyList.each(function (i, elem) { // each는 배열을 순회하며 callnack 함수를 실행하는 함수.
             ulList[i] = {
-                bookList: $(this).find('a').text(),
-                url: $(this).find('a').attr('href'),
+                bookList: $(this).find('a').text(), //<a> 태그 안의 텍스트만 뽑는다.
+                url: $(this).find('a').attr('href'), // attr()는 해당 속성 안에 들어있는 텍스트를 뽑는다.
             };
         });
 
