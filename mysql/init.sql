4efb3e0200780201223ef03cc80227038bbab432
SuhHyeonjun <dmsgktn0208@gmail.com>
2022-08-09 18:40:49 +0900

    UPDATE

--- mysql/init.sql
+++
@@ -1,6 +0,0 @@
-insert into shop.customers(name, age, sex) values('김무진', 32, '남');
-insert into shop.customers(name, age, sex) values('이영애', 24, '여');
-insert into shop.customers(name, age, sex) values('이동훈', 28, '남');
-insert into shop.customers(name, age, sex) values('김슬혜', 35, '여');
-
-select * from customers;
