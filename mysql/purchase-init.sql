d8496ce4f652c4e9421b113187c48f5ddd66e61c
SuhHyeonjun <dmsgktn0208@gmail.com>
2022-08-09 17:06:03 +0900

    purchase 데이터 init

---
+++ mysql/purchase-init.sql
@@ -0,0 +1,7 @@
+insert into shop.purchase(customer_id, book_name) values('1', 'DevOps 기초');
+insert into shop.purchase(customer_id, book_name) values('2', 'Node.js 입문');
+insert into shop.purchase(customer_id, book_name) values('3', 'Javascripts 공부하기');
+insert into shop.purchase(customer_id, book_name) values('3', '머신러닝 입문하기');
+insert into shop.purchase(customer_id, book_name) values('4', 'CS면접 공부하기');
+
+select * from purchase;
