0e0fc4e852f3d6faffc44af0c0c2c1be75bee4f6
SuhHyeonjun <dmsgktn0208@gmail.com>
2022-08-10 13:41:10 +0900

    ADD:asnyc/await

--- mysql/app.js
+++ mysql/app.js
@@ -1,11 +1,14 @@
 const { sequelize } = require('./models/index.js');
 
-const driver = () => {
-    sequelize.sync().then(() => {
-        console.log('초기화 완료');
-    }).catch((err) => {
-        console.error('초기화 실패');
-        console.error(err);
-    });
-};
+const driver = async () => {
+    try {
+        await sequelize.sync()
+    } catch(err) {
+            console.error('초기화 실패');
+            console.error(err);
+            return;
+    }       
+    console.log('초기화 완료');
+    };
+
 driver();
